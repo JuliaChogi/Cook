99d1dc50bf7a77101b358998f0e53521e4f16d31
JuliaChogi <jwolf2019@yandex.ru>
2022-06-07 10:50:22 +0300

    1111
    1111

---
+++ src/backend/server.js
@@ -0,0 +1,24 @@
+const fs = require('fs');
+const http = require ('http');
+const PORT = 3000;
+
+const server = http.createServer((req, res) => {
+    res.writeHead(200, {
+        'Content-Type': 'text/html'
+    })
+
+    fs.readFile('../CookProject/src/frontend/index.html', (err, data) => {
+        err
+        ? res.writeHead(404) && res.write('Error: File not Found')
+        : res.write(data)
+    
+    })
+})
+
+
+
+server.listen(PORT, err => {
+    err
+    ? console.log(`Something went wrong`, err)
+    : console.log(`Server is listening on port: ${PORT}`);
+})
